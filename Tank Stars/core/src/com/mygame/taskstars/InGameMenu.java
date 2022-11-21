1a53eecc0b02878bcbe7f048f2f847cf2c4020a1
Abhijeet Anand <108174938+AbhijeetAnand23@users.noreply.github.com>
2022-11-22 00:41:08 +0530

    Abhijeet Deadline 1 Done

--- Tank Stars/core/src/com/mygame/taskstars/InGameMenu.java
+++ Tank Stars/core/src/com/mygame/taskstars/InGameMenu.java
@@ -1,15 +1,25 @@
 package com.mygame.taskstars;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
 import com.mygame.taskstars.TankStars;
+
+import java.awt.*;
 
 public class InGameMenu implements Screen {
     TankStars game;
     SpriteBatch batch;
     Texture background;
+    Rectangle closeBox;
+    Vector3 closeTouch;
+    Rectangle resumeBox;
+    Vector3 resumeTouch;
+    Rectangle mainMenuBox;
+    Vector3 mainMenuTouch;
     ShapeRenderer shapeRenderer;
 
     public InGameMenu(TankStars game) {
@@ -18,6 +28,9 @@ public class InGameMenu implements Screen {
         shapeRenderer = new ShapeRenderer();
 
         background = new Texture("InGameMenu.png");
+        closeBox = new Rectangle(1240,700, 44,44);
+        resumeBox = new Rectangle(895, 595, 198, 69);
+        mainMenuBox = new Rectangle(895, 178, 198, 69);
     }
     @Override
     public void show() {
@@ -27,8 +40,41 @@ public class InGameMenu implements Screen {
     @Override
     public void render(float delta) {
         game.batch.begin();
-        game.batch.draw(background, 315,90, 761, 700);
+        game.batch.draw(background, 630,90, 761, 700);
         game.batch.end();
+
+        if (Gdx.input.justTouched()) {
+            closeTouch = new Vector3();
+            closeTouch.set(Gdx.input.getX(), 915 - Gdx.input.getY(), 0);
+            if(closeBox.contains(closeTouch.x, closeTouch.y)) {
+                game.setScreen(new VsFriend(game));
+                dispose();
+            }
+        }
+
+        if (Gdx.input.justTouched()) {
+            resumeTouch = new Vector3();
+            resumeTouch.set(Gdx.input.getX(), 915 - Gdx.input.getY(), 0);
+            if(resumeBox.contains(resumeTouch.x, resumeBox.y)) {
+                game.setScreen(new VsFriend(game));
+                dispose();
+            }
+        }
+
+        if (Gdx.input.justTouched()) {
+            mainMenuTouch = new Vector3();
+            mainMenuTouch.set(Gdx.input.getX(), 915 - Gdx.input.getY(), 0);
+            if(mainMenuBox.contains(mainMenuTouch.x, mainMenuTouch.y)) {
+                game.setScreen(new MainMenu(game));
+                dispose();
+            }
+        }
+
+//        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+//        shapeRenderer.rect(closeBox.x, closeBox.y, closeBox.width, closeBox.height);
+//        shapeRenderer.rect(resumeBox.x, resumeBox.y, resumeBox.width, resumeBox.height);
+//        shapeRenderer.rect(mainMenuBox.x, mainMenuBox.y, mainMenuBox.width, mainMenuBox.height);
+//        shapeRenderer.end();
     }
 
     @Override
