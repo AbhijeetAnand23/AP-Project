98d001a26e331cf2f87223fb467a57190e4e180a
mananchugh02 <99235312+mananchugh02@users.noreply.github.com>
2022-12-20 14:34:24 +0530

    ground added

--- Project/Tank Stars/core/src/com/mygame/taskstars/NewGame.java
+++ Project/Tank Stars/core/src/com/mygame/taskstars/NewGame.java
@@ -2,9 +2,12 @@ package com.mygame.taskstars;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.awt.*;
@@ -20,6 +23,11 @@ public class NewGame implements Screen {
     private Texture tank2;
     private Rectangle menuBox;
     private Vector3 menuTouch;
+    private World world;
+    private Body box;
+    private Box2DDebugRenderer debugRenderer;
+
+    private OrthographicCamera camera;
     public NewGame(TankStars game){
         this.game = game;
         batch = new SpriteBatch();
@@ -31,6 +39,19 @@ public class NewGame implements Screen {
         tank1 = new Texture("Tank1.png");
         tank2 = new Texture("Tank2 Reversed.png");
         menuBox = new Rectangle(63,820,60,70);
+        debugRenderer = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth()/4, Gdx.graphics.getHeight()/4);
+        world = new World(new Vector2(0,-9.8f), true);
+//        box = new Body(world, );
+        BodyDef bodyDef = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+        ChainShape chainShape = new ChainShape();
+        fixtureDef.shape = chainShape;
+        chainShape.createChain(new Vector2[]{new Vector2(-250, 55), new Vector2(-190,55), new Vector2(-157,-8),
+                 new Vector2( -82,-8), new Vector2(-63, 26), new Vector2(-14, 26), new Vector2(21,-42),
+                 new Vector2(98,-42), new Vector2(118,24), new Vector2(191, 24), new Vector2(212,-18),
+                 new Vector2(250, -18)});
+        world.createBody(bodyDef).createFixture(fixtureDef);
 
     }
     @Override
@@ -49,6 +70,9 @@ public class NewGame implements Screen {
         game.batch.draw(tank1,370,381,170,170);
         game.batch.draw(tank2, 1475,505,170,170);
         game.batch.end();
+
+        debugRenderer.render(world, camera.combined);
+        world.step(1/60f,8,3);
 
         if (Gdx.input.justTouched()) {
             menuTouch = new Vector3();
