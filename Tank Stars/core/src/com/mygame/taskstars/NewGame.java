594f6da59b33d9c208c204d2c232e64b4c5b7bef
mananchugh02 <99235312+mananchugh02@users.noreply.github.com>
2022-12-21 19:56:59 +0530

    Add files via upload

--- Tank Stars/core/src/com/mygame/taskstars/NewGame.java
+++ Tank Stars/core/src/com/mygame/taskstars/NewGame.java
@@ -24,6 +24,7 @@ public class NewGame implements Screen {
     private Vector3 menuTouch;
     private Player player1;
     private Player player2;
+    private int flag =0;
 
     public NewGame(TankStars game){
         this.game = game;
@@ -34,11 +35,11 @@ public class NewGame implements Screen {
         health = new Texture("Health.png");
         menuIcon = new Texture("MenuIcon.png");
 //        tank1 = new Texture("Tank1.png");
-        player1 = new Player("tank1.png",new Vector2(370, 340),new Vector2(170,170));
-        player2 = new Player("Tank2 Reversed.png", new Vector2(1475, 336),new Vector2(170,170));
+        player1 = new Player("tank1.png",new Vector2(370, 340),new Vector2(170,170),"Fuel Bar.png", "Fuel_BG.png","Health_Bar.png", "Health_box.png");
+        player2 = new Player("Tank2 Reversed.png", new Vector2(1475, 336),new Vector2(170,170),"Fuel Bar.png", "Fuel_BG.png","Health_Bar.png", "Health_box.png");
+
 //        tank2 = new Texture("Tank2 Reversed.png");
         menuBox = new Rectangle(63,820,60,70);
-
     }
     @Override
     public void show() {
@@ -51,13 +52,11 @@ public class NewGame implements Screen {
         game.batch.begin();
         game.batch.draw(background, 0,0,1900, 915);
         game.batch.draw(terrain,0,-150,1920,1080);
-        game.batch.draw(health, -400,720,2798, 256);
+//        game.batch.draw(health, -400,720,2798, 256);
         game.batch.draw(menuIcon, 0,790,188, 138);
 
 //        game.batch.draw(tank1,370,340,170,170);
 //        game.batch.draw(tank2, 1475,336,170,170);
-
-
         if (Gdx.input.justTouched()) {
             menuTouch = new Vector3();
             menuTouch.set(Gdx.input.getX(), 915 - Gdx.input.getY(), 0);
@@ -66,44 +65,93 @@ public class NewGame implements Screen {
                 dispose();
             }
         }
-        if(Gdx.input.isKeyJustPressed(Input.Keys.D)){
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
             float x = player1.getX()+2;
             float y = player1.getY();
             float fuel = player1.getFuel()-1;
-            player1.dispose();
-            player1 = new Player("tank1.png",new Vector2(x, y),new Vector2(170,170));
-            player1.setFuel(fuel);
-            player1.draw(game.batch);
+            if(fuel>=0) {
+                player1.dispose();
+                player1 = new Player("tank1.png", new Vector2(x, y), new Vector2(170, 170), "Fuel Bar.png", "Fuel_BG.png","Health_Bar.png", "Health_box.png");
+                player1.setFuel(fuel);
+                player1.draw(game.batch);
+            }
         }
-        if(Gdx.input.isKeyJustPressed(Input.Keys.A)){
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
             float x = player1.getX()-2;
             float y = player1.getY();
             float fuel = player1.getFuel()-1;
-            player1.dispose();
-            player1 = new Player("tank1.png",new Vector2(x, y),new Vector2(170,170));
-            player1.setFuel(fuel);
-            player1.draw(game.batch);
+            if(fuel>=0) {
+                player1.dispose();
+                player1 = new Player("tank1.png", new Vector2(x, y), new Vector2(170, 170), "Fuel Bar.png", "Fuel_BG.png","Health_Bar.png", "Health_box.png");
+                player1.setFuel(fuel);
+                player1.draw(game.batch);
+            }
         }
-        if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
             float x = player2.getX()+2;
             float y = player2.getY();
-            float fuel = player2.getFuel();
-            player2.dispose();
-            player2 = new Player("Tank2 Reversed.png",new Vector2(x, y),new Vector2(170,170));
-            player2.setFuel(fuel);
-            player2.draw(game.batch);
+            float fuel = player2.getFuel()-1;
+            if(fuel>=0) {
+                player2.dispose();
+                player2 = new Player("Tank2 Reversed.png", new Vector2(x, y), new Vector2(170, 170), "Fuel Bar.png", "Fuel_BG.png","Health_Bar.png", "Health_box.png");
+                player2.setFuel(fuel);
+                player2.draw(game.batch);
+            }
         }
-        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
             float x = player2.getX()-2;
             float y = player2.getY();
-            float fuel = player2.getFuel();
-            player2.dispose();
-            player2 = new Player("Tank2 Reversed.png",new Vector2(x, y),new Vector2(170,170));
-            player2.setFuel(fuel);
-            player2.draw(game.batch);
+            float fuel = player2.getFuel() - 1;
+            if(fuel>=0) {
+                player2.dispose();
+                player2.fuelDispose();
+                player2 = new Player("Tank2 Reversed.png", new Vector2(x, y), new Vector2(170, 170), "Fuel Bar.png", "Fuel_BG.png","Health_Bar.png", "Health_box.png");
+                player2.setFuel(fuel);
+                player2.draw(game.batch);
+            }
+        }
+        if(flag == 0){
+            if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)){
+                double fall_x = player1.attack1();
+                System.out.println("1 attacked at " + fall_x);
+                flag = 1;
+                player1.setAngle(30);
+            }
+            if(Gdx.input.isKeyJustPressed(Input.Keys.W)){
+                double angle = player1.getAngle() + 10;
+                System.out.printf("Angle++");
+                player1.setAngle(angle);
+            }
+            if(Gdx.input.isKeyJustPressed(Input.Keys.S)){
+                double angle = player1.getAngle() - 10;
+                System.out.printf("Angle--");
+                player1.setAngle(angle);
+            }
+        }
+        else if(flag == 1){
+            if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)){
+                double fall_x = player2.attack2();
+                System.out.println("2 attacked at " + fall_x);
+                flag = 0;
+                player2.setAngle(30);
+            }
+            if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+                double angle = player2.getAngle() + 10;
+                System.out.printf("Angle++");
+                player2.setAngle(angle);
+            }
+            if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+                double angle = player2.getAngle() - 10;
+                System.out.printf("Angle--");
+                player2.setAngle(angle);
+            }
         }
         player1.draw(game.batch);
         player2.draw(game.batch);
+        player1.draw_Fuel1(game.batch,player1.getFuel());
+        player2.draw_Fuel2(game.batch, player2.getFuel());
+        player1.draw_healthBar1(game.batch, 50);
+        player2.draw_healthBar2(game.batch, 50);
         game.batch.end();
     }
 
--- Tank Stars/core/src/com/mygame/taskstars/Player.java
+++ Tank Stars/core/src/com/mygame/taskstars/Player.java
@@ -9,12 +9,21 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 public class Player extends Image {
     Vector2 position, size;
     Texture player;
-    float fuel = 20;
+    Texture fuel_box;
+    Texture fuel_bar;
+    Texture health_box;
+    Texture health_bar;
+    float fuel = 200;
     float health = 100;
-    float angle;
-    float velocity;
+    double angle = 30;
+    double velocity = 155;
 
-    public Player(String file, Vector2 position, Vector2 size){
+    public Player(String file, Vector2 position, Vector2 size, String file1, String file2, String file3, String file4){
+        fuel_box  = new Texture(file2);
+        fuel_bar  = new Texture(file1);
+//        heart = new Texture(file5);
+        health_bar = new Texture(file3);
+        health_box = new Texture(file4);
         player = new Texture(file);
         this.position = position;
         this.size = size;
@@ -22,7 +31,48 @@ public class Player extends Image {
     public void draw(SpriteBatch batch){
         batch.draw(player, position.x, position.y, size.x, size.y);
     }
-
+    public void draw_Fuel1(SpriteBatch batch, float x){
+        batch.draw(fuel_box, 30, 40, 200, 50);
+        batch.draw(fuel_bar, 30, 40, x, 50);
+    }
+    public void fuelDispose(){
+        fuel_box.dispose();
+        fuel_bar.dispose();
+    }
+    public void draw_Fuel2(SpriteBatch batch, float x){
+        batch.draw(fuel_box, 1650, 40, 200, 50);
+        batch.draw(fuel_bar, 1650, 40, x, 50);
+    }
+    public void draw_healthBar1(SpriteBatch batch, float x){
+        batch.draw(health_box, 500, 800, 300, 75);
+        batch.draw(health_bar, 500, 800, 150, 75);
+    }
+    public void draw_healthBar2(SpriteBatch batch, float x){
+        batch.draw(health_box, 1090, 800, 300, 75);
+        batch.draw(health_bar, 1090, 800, 150, 75);
+    }
+    public double attack1(){
+        double range;
+        double a = Math.toRadians(angle);
+        double sin = Math.sin(2*a);
+        range = (velocity*velocity*sin) / (2*9.8);
+        double fall_position = position.x + range;
+        return fall_position;
+    }
+    public double attack2(){
+        double range;
+        double a = Math.toRadians(angle);
+        double sin = Math.sin(2*a);
+        range = (velocity*velocity*sin) / (2*9.8);
+        double fall_position = position.x - range;
+        return fall_position;
+    }
+    public void setAngle(double angle){
+        this.angle = angle;
+    }
+    public double getAngle(){
+        return this.angle;
+    }
     public Vector2 getPosition() {
         return position;
     }
